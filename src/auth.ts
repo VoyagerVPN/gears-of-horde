bf4c41ec145a7a2510e9ba079909c0c0227b1390
VoyagerVPN <christinethorntongm8je7@gmail.com>
2025-12-27 11:12:14 +0300

    Disabled Debug

--- src/auth.ts
+++ src/auth.ts
@@ -23,5 +23,5 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             return session
         },
     },
-    debug: true,
+    debug: false,
 })
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -21,7 +21,7 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 export const db =
     globalForPrisma.prisma || new PrismaClient({
         adapter,
-        log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error']
+        log: ['error']
     })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
